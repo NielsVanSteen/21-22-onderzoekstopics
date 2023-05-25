80bd32d036ddf7b3325b294c9f10e67832fd1798
Niels Van Steen <niels.vansteen@student.kdg.be>
2023-05-25 15:23:10 +0200

    project push to github

---
+++ SoftwareApplicationManager/Domain/Rating.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SoftwareApplicationManager.BL.Domain
+{
+    public class Rating
+    {
+        // Properties.
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Range(0, 10)]
+        public double Score { get; set; }
+        public DateTime PostedOn { get; set; }
+        
+        [Required]
+        public Developer Developer { get; set; }
+        [Required]
+        public SoftwareApplication SoftwareApplication { get; set; }
+
+        // Constructor.
+        public Rating() {}
+        public Rating(string title, double score)
+        {
+            Title = title;
+            Score = score;
+        } // Rating.
+
+        public Rating(string title, double score, DateTime postedOn, string description = null)
+        {
+            Title = title;
+            Description = description;
+            Score = score;
+            PostedOn = postedOn;
+        } // Rating.
+        
+        public Rating(string title, double score, DateTime postedOn, Developer developer, SoftwareApplication softwareApplication, string description = null) : this(title, score, postedOn, description)
+        {
+            Developer = developer;
+            SoftwareApplication = softwareApplication;
+        } // Rating.
+        
+        // Methods.
+        
+        
+    }
+}
